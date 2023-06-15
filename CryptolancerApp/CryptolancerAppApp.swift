240169f42b875805c39596d545faecba1c7b494d
obli137 <obli137@gmail.com>
2023-06-15 16:55:13 -0300

    Initial Commit

---
+++ CryptolancerApp/CryptolancerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptolancerAppApp.swift
+//  CryptolancerApp
+//
+//  Created by Mariano Obligado on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptolancerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
